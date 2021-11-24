415724e4a0e1abb1ae96f5c60f899edf992865fe
George Lambert <george.lambert99@gmail.com>
2021-11-24 17:40:30 +0000

    Added shrinking Navbar, need to workout how to alter some aspects in progress.

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-console.log('hello!')
+window.onscroll = function() {scrollFunction()};
 
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.height = "2em";
+    document.getElementById("nametag").style.fontSize = "1em";
+    document.getElementById("navbar-right").style.fontSize = "0.75em";
+    document.getElementById("navbar-right").style.padding = "0.5em 1em";
+  } else {
+    document.getElementById("navbar").style.height = "4em";
+    document.getElementById("nametag").style.fontSize = "1.75em";
+    document.getElementById("navbar-right").style.fontSize = "1em";
+    document.getElementById("navbar-right").style.padding = "1em 1em";
+    
+  }
+}
