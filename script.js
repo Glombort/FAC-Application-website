95a7470a0406ddfa67f26042b532973ea6d41707
George Lambert <george.lambert99@gmail.com>
2021-12-01 15:09:33 +0000

    Added new pages and pic/bio to intro

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.height = "2em";
-    document.getElementById("nametag").style.fontSize = "1em";
-    document.getElementById("navbar-right").style.fontSize = "0.75em";
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    document.getElementById("navbar").style.height = "3em";
+    document.getElementById("nametag").style.fontSize = "1.25em";
+    document.getElementById("navbar-right").style.fontSize = "0.95em";
     document.getElementById("navbar-right").style.padding = "0.5em 1em";
   } else {
     document.getElementById("navbar").style.height = "4em";
     document.getElementById("nametag").style.fontSize = "1.75em";
-    document.getElementById("navbar-right").style.fontSize = "1em";
+    document.getElementById("navbar-right").style.fontSize = "1.1em";
     document.getElementById("navbar-right").style.padding = "1em 1em";
     
   }
