2229fdfc4e4bd9769b314c1759e1b7bf150ae5da
Glombort <george.lambert99@gmail.com>
2021-12-03 15:13:13 +0000

    Nav and Contacts in JS so auto update for all page

--- js/script.js
+++ js/script.js
@@ -14,3 +14,49 @@ function scrollFunction() {
     
   }
 }
+
+
+//Navbar same on each page
+var nav = `
+<header>
+  <h1 id="nametag"><a href=".\\index.html">George Lambert</a></h1>
+</header>
+<nav id="navbar-right">
+  <a href=".\\about.html">About Me</a> |
+  <div class="dropdown">
+    <a href=".\\projects.html">Projects
+      <div id="project-list">
+        <a href=".\\projects.html#functions">Functions</a>
+        <a href=".\\projects.html#number-functions">Number Functions</a>
+        <a href=".\\projects.html#dom">DOM</a>
+        <a href=".\\projects.html#array">Array</a>
+        <a href="https://www.freecodecamp.org/glamb3rt" target="_blank">FreeCodeCamp</a>
+        <a href="https://github.com/Glombort/foundersAndCoders" target="_blank">GitHub</a>
+      </div>
+    </a> |
+  </div>
+  <a href=".\\applying.html">Why I'm Applying</a>
+</nav>`;
+
+document.getElementById("navbar").innerHTML = nav;
+
+// Contacts Section same on each page.
+var contacts = `
+<header>
+  <h1>Contact</h1>
+</header>
+<div id="contact-links">
+  <ul>
+    <li>
+      &#9993; <a href="mailto:george.lambert99@gmail.com">george.lambert99@gmail.com</a>
+    </li>
+    <li>
+      LinkedInLogo <a href="https://www.linkedin.com/in/george-lambert-986929187/" target="_blank">George LinkedIn</a>
+    </li>
+    <li>
+      GitHubLogo <a href="https://github.com/Glombort" target="_blank">GitHub</a>
+    </li>
+  </ul>
+</div>`;
+
+document.getElementById('foot').innerHTML = contacts
