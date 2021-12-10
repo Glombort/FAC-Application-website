a3a06b93dd448528b6756c7f178750f0bbced056
Glombort <george.lambert99@gmail.com>
2021-12-10 13:30:12 +0000

    Project page with links

--- prompts/arrays/script.js
+++ prompts/arrays/script.js
@@ -1,40 +1,27 @@
 document.getElementById("submit").onclick = function() {
 
     var array = document.getElementById("user-input").value;
-    array=array.split(",").map(function (number) {
-        return parseInt(number,10);
-    });
-
+    array = array.split(",").map((number) => parseInt(number,10))
+    //used the .split method to create an array split at each comma
+    //used the .map method to go through the array keeping only numbers
     var method = document.getElementsByName("array-method");
 
     if ( method[0].checked) {
-        filter(array)
-    } else {
-        reduce(array)
-    }
-}
-
-
-function filter(array) {
-    var returnArr =[]
-    for (let i=0; i < array.length; i++) {
-        if (array[i] % 2 == 0) {
-            returnArr.push(array[i]);
+        //filter method returns only numbers divisible by two
+        array = array.filter(number => (number % 2 == 0))
+        if (array.length > 0) {
+            document.getElementById("output-p").innerHTML = "Here is your array filtered with just numbers divisible by two, [" + array + "]"
+        } else {
+            document.getElementById("output-p").innerHTML = "Your array has no numbers divisible by two"
         }
-    }
-    if (returnArr.length > 0) {
-        document.getElementById("output-p").innerHTML = "Here is your array filtered with just [" + returnArr + "]"
     } else {
-        document.getElementById("output-p").innerHTML = "Your array has no numbers divisible by two"
+        //reduce method takes a running total as it parses through the array adding on the current element.
+        const returnSum = array.reduce(function sum(accumulated, current) {
+            if(!isNaN(current)) {
+                accumulated += current
+            }
+            return accumulated
+        }, 0)
+        document.getElementById("output-p").innerHTML = "The sum of your input is " + returnSum
     }
-}
-
-function reduce(array) {
-    var returnSum = 0
-    for (let i=0; i < array.length; i++) {
-        if (!isNaN(array[i]) ) {
-            returnSum +=array[i];
-        }
-    }
-    document.getElementById("output-p").innerHTML = "The sum of your input is " + returnSum
 }
